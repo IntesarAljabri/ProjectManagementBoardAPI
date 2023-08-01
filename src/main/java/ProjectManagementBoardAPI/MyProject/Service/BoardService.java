2d2d17bb63fbda2f138b18ce94a935d93346ded6
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 11:32:52 +0400

    add new board

--- src/main/java/ProjectManagementBoardAPI/MyProject/Service/BoardService.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Service/BoardService.java
@@ -17,38 +17,7 @@ public class BoardService {
     public Board addBoard(Board board) {
         return boardRepository.save(board);
     }
-    //get all Board
-    public List<Board> getAllBoard() {
-        return boardRepository.findAll();
-    }
-
-    //get Board by id
-    public Board getBoardById(Integer id) {
-        return boardRepository.findById(id).get();
-    }
-
-    public Board createBoard(Board board) {
-        return boardRepository.save(board);
-    }
-
-    //delete Board
-    public String deleteBoard(Integer id) {
-        boardRepository.deleteById(id);
-        return "Deleted  Successfully";
-    }
-
-    //update information about Teacher
-    public Board updateBoard(Integer id, Board updateData){
-        Board board = boardRepository.findById(id).orElse(null);
-
-        if (board != null) {
-            board.setId(updateData.getId());
-            board.setTitle(updateData.getTitle());
-
-            return boardRepository.save(board);
-        }
-        return null;
-    }
+  
 
 
 }
