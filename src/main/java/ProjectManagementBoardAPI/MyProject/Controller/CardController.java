07b8a4cf2e5a31a507cfaae48999329186fc3c36
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 15:58:08 +0400

    Update Card Information in CardController

--- src/main/java/ProjectManagementBoardAPI/MyProject/Controller/CardController.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Controller/CardController.java
@@ -5,6 +5,7 @@ import ProjectManagementBoardAPI.MyProject.Model.Card;
 import ProjectManagementBoardAPI.MyProject.Repository.CardRepository;
 import ProjectManagementBoardAPI.MyProject.Service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,32 +14,50 @@ import java.util.List;
 @RequestMapping("/cards")
 public class CardController {
 
-    @Autowired
-    CardService cardService;
+        @Autowired
+        CardService cardService;
 
-    // Add new Card
-    @PostMapping(value = "add")
-    public String Card(@RequestBody Card card) {
-        cardService.addCard(card);
-        return "Card added";
-    }
+        // Add new Card
+        @PostMapping(value = "add")
+        public String Card(@RequestBody Card card) {
+            cardService.addCard(card);
+            return "Card added";
+        }
 
-    //Get All Card
-    @GetMapping(value = "getAll")
-    public List<Card> getAllCard() {
-        return cardService.getAllCard();
-    }
+        //Get All Card
+        @GetMapping(value = "getAll")
+        public List<Card> getAllCard() {
+            return cardService.getAllCard();
+        }
 
-    //Get Card by Id
-    @GetMapping(value = "getById")
-    public Card getCardById(Integer id) {
-        return cardService.getCardById(id);
-    }
+        //Get Card by Id
+        @GetMapping(value = "getById")
+        public Card getCardById(Integer id) {
+            return cardService.getCardById(id);
+        }
 
-    // Delete Card by id
-    @DeleteMapping("/{id}")
-    public String deleteCard(@PathVariable Integer id) {
-        cardService.deleteCard(id);
-        return "Deleted Successfully";
-    }
+        // Delete Card by id
+        @DeleteMapping("/{id}")
+        public String deleteCard(@PathVariable Integer id) {
+            cardService.deleteCard(id);
+            return "Deleted Successfully";
+        }
+
+         //Update card information
+        @PutMapping("/{boardId}/cards/{cardId}")
+        public ResponseEntity<Object> updateCard(
+                @PathVariable Integer boardId,
+                @PathVariable Integer cardId,
+                @RequestBody Card card
+        ) {
+            if (!card.getId().equals(cardId)) {
+                return ResponseEntity.badRequest().build();
+            }
+            card.setId(cardId);
+            Board updatedBoard = cardService.updateCard(card);
+            if (updatedBoard != null) {
+                return ResponseEntity.ok(updatedBoard);
+            }
+            return ResponseEntity.notFound().build();
+        }
 }
