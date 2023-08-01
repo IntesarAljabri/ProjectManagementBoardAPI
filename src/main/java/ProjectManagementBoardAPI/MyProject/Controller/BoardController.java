a85b79c661b7d8cdec1bd8c1c353d7190d3739ad
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 12:05:22 +0400

    addBoard in boardController

--- src/main/java/ProjectManagementBoardAPI/MyProject/Controller/BoardController.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Controller/BoardController.java
@@ -1,7 +1,10 @@
 package ProjectManagementBoardAPI.MyProject.Controller;
 
+import ProjectManagementBoardAPI.MyProject.Model.Board;
 import ProjectManagementBoardAPI.MyProject.Service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,11 @@ public class BoardController {
 
     @Autowired
     BoardService boardService;
+
+    // Add new board
+    @PostMapping(value = "add")
+    public String Board(@RequestBody Board board) {
+        boardService.addBoard(board);
+        return "Board added";
+    }
 }
