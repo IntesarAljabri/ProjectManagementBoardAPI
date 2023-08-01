c5462cafbe732a699fc406d805aa6737d1bc17c6
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 14:54:38 +0400

    add prporty of cardResponse

--- src/main/java/ProjectManagementBoardAPI/MyProject/Responce/CardResponse.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Responce/CardResponse.java
@@ -1,4 +1,16 @@
 package ProjectManagementBoardAPI.MyProject.Responce;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CardResponse {
+
+    Integer id;
+    String title;
+    String description;
+    String section;
 }
