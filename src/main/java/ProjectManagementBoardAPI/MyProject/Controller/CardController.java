4980898bdb9bb23913a1225ef93e84508853dd6f
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 15:41:42 +0400

    GetAllCard in CardController

--- src/main/java/ProjectManagementBoardAPI/MyProject/Controller/CardController.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Controller/CardController.java
@@ -5,10 +5,9 @@ import ProjectManagementBoardAPI.MyProject.Model.Card;
 import ProjectManagementBoardAPI.MyProject.Repository.CardRepository;
 import ProjectManagementBoardAPI.MyProject.Service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cards")
@@ -22,5 +21,11 @@ public class CardController {
     public String Card(@RequestBody Card card) {
         cardService.addCard(card);
         return "Card added";
+    }
+
+    //Get All Card
+    @GetMapping(value = "getAll")
+    public List<Card> getAllCard() {
+        return cardService.getAllCard();
     }
 }
