a23f20e4c2f4792cb9fe6379eb6797eb6c77b7d5
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 14:33:58 +0400

    create the card model aand check borad service

---
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Model/Card.java
@@ -0,0 +1,23 @@
+package ProjectManagementBoardAPI.MyProject.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Data
+@Table(name = "Card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Integer id;
+    String title;
+    String description;
+    String section;
+
+}
--- src/main/java/ProjectManagementBoardAPI/MyProject/Service/BoardService.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Service/BoardService.java
@@ -13,42 +13,45 @@ public class BoardService {
     @Autowired
     BoardRepository boardRepository;
 
-    //add new board
-    public Board addBoard(Board board) {
-        return boardRepository.save(board);
-    }
-    //get all Board
-    public List<Board> getAllBoard() {
-        return boardRepository.findAll();
-    }
-
-
-    //get Board by id
-    public Board getBoardById(Integer id) {
-        return boardRepository.findById(id).get();
-    }
-
-    public Board createBoard(Board board) {
-        return boardRepository.save(board);
-    }
-
-    //delete Board
-    public String deleteBoard(Integer id) {
-        boardRepository.deleteById(id);
-        return "Deleted  Successfully";
-    }
-
-    //update information about Board
-    public Board updateBoard(Board updateData){
-        Board board = boardRepository.findById(id).orElse(null);
-
-        if (board != null) {
-            board.setId(updateData.getId());
-            board.setTitle(updateData.getTitle());
-
+        //add new board
+        public Board addBoard(Board board) {
             return boardRepository.save(board);
         }
-        return null;
-    }
+        //get all Board
+        public List<Board> getAllBoard() {
+            return boardRepository.findAll();
+        }
+
+
+        //get Board by id
+        public Board getBoardById(Integer id) {
+            return boardRepository.findById(id).get();
+        }
+
+        public Board createBoard(Board board) {
+            return boardRepository.save(board);
+        }
+
+        //delete Board
+        public String deleteBoard(Integer id) {
+            boardRepository.deleteById(id);
+            return "Deleted  Successfully";
+        }
+
+         // Update information about Board
+         public Board updateBoard(Board updateData) {
+             Board board = boardRepository.findById(updateData.getId()).orElse(null);
+             if (board != null) {
+                 if (updateData.getId() != null) {
+                     board.setId(updateData.getId());
+                 }
+                 if (updateData.getTitle() != null) {
+                     board.setTitle(updateData.getTitle());
+                 }
+
+                 return boardRepository.save(board);
+             }
+             return null;
+         }
 
 }
