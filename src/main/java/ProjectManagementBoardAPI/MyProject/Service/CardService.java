8d70cf8f0b827e48e798dceb5cc3a878773cf2cb
Maan94919411* <intesaraljabri99@gmail.com>
2023-08-01 15:18:59 +0400

    Get all card

--- src/main/java/ProjectManagementBoardAPI/MyProject/Service/CardService.java
+++ src/main/java/ProjectManagementBoardAPI/MyProject/Service/CardService.java
@@ -6,6 +6,8 @@ import ProjectManagementBoardAPI.MyProject.Repository.CardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CardService {
 
@@ -13,8 +15,13 @@ public class CardService {
     CardRepository cardRepository;
 
 
-    //add new board
+    //add new Card
     public Card addCard(Card card) {
         return cardRepository.save(card);
+    }
+
+    //get all Card
+    public List<Card> getAllCard() {
+        return cardRepository.findAll();
     }
 }
